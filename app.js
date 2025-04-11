789877c0f0c9a2c05adc2fcffd0c85c6595b686b
nbthinh <nguyenbuuthinh036@gmail.com>
2025-04-11 20:11:46 +0700

    first commit: video 1

---
+++ app.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+const expressLayout = require("express-ejs-layouts");
+const PORT = process.env.PORT | 8080;
+app.use(express.static("public"))
+app.use(expressLayout);
+app.set("layout", "./layouts/main");
+app.set("view engine", "ejs");
+
+app.use("/", require("./server/routes/main"));
+// app.get("", (req, res) => {
+//     res.send("Hello world");
+// });
+app.listen(PORT, () => {
+    console.log(`App is listening on port ${PORT}`)
+})
