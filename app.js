d10381bbbcbb0d1c94e8b68ff611ffb254cfb9b2
Nguyễn Bửu Thịnh <nguyenbuuthinh@Nguyens-MacBook-Pro.local>
2025-04-19 18:50:31 +0700

    ok

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 require("dotenv").config();
 const express = require("express");
-
+const connectDB = require("./server/config/db");
 const app = express();
 const expressLayout = require("express-ejs-layouts");
 const PORT = process.env.PORT | 8080;
+
+// Connect DB
+connectDB();
 app.use(express.static("public"))
 app.use(expressLayout);
 app.set("layout", "./layouts/main");
