353aa91f8da68f7e34d4e8fba977e4b1014b31ea
nbthinh <nguyenbuuthinh036@gmail.com>
2025-04-22 11:11:18 +0700

    finish video 8

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -6,6 +6,26 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const jwtSecret = process.env.JWT_SECRET
 const adminLayout = '../views/layouts/admin';
+
+const authMiddleware = (req, res, next) => {
+    const token = req.cookies.token;
+    if (!token) {
+        return res.status(401).json({
+            message: "Unauthorized"
+        })
+    }
+    try {
+        const decoded = jwt.verify(token, jwtSecret);
+        req.userId = decoded.userId;
+        next();
+    }
+    catch(error) {
+        return res.status(401).json({
+            message: "Unauthorized"
+        })
+    }
+}
+
 router.get("/admin", async (req, res) => {
     
     try {
